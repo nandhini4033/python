28931e7930172cbf75c84db8aa6a13d04409bf5f
nandhini4033 <nandhiniscse@gmail.com>
2014-01-20 18:39:52 +0530

    Create file.py

---
+++ file.py
@@ -0,0 +1,17 @@
+
+class student:
+ name=" "
+ mark=0
+ def.display(self):
+  print("student name is {0}\n student mark is {1}".format(self.name,self.mark))
+obj=student("nandhini",100)
+obj.display()
+class sports(student)
+ game="Athlet"
+ rank=20
+ def.play(self)
+  print("play the game",self.game)
+  print("rank",self.rank)
+s=sports()
+s.display()
+s.play()
